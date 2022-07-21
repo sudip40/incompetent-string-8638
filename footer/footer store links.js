1ae9a56f0dc3f1359553d8a15175678fbbdbd103
sushantkr961 <sushantonly961@gmail.com>
2022-07-22 00:32:02 +0530

    done!

---
+++ footer/footer store links.js
@@ -0,0 +1,50 @@
+function footerLinks() {
+    return `<div id="footer_links">
+    <div id="sks_outer">
+        <div id="sk_txt">
+            <span>GET OUR MOBILE APP</span>
+            <h2>Download and start your workout journey</h2>
+        </div>
+        <div id="sk_qr">
+            <img src="https://cdn.jefit.com/wp/wp-content/uploads/2021/12/qrcode_only.png">
+        </div>
+        <div id="sk_li">
+            <a href="https://play.google.com/store/apps/details?id=je.fit&referrer=utm_source%3Demail"><img src="https://cdn.jefit.com/wp/wp-content/uploads/2021/11/googleplay.png.webp" alt="play store"></a>
+            <a href="https://apps.apple.com/app/apple-store/id449810000"><img src="https://cdn.jefit.com/wp/wp-content/uploads/2021/11/appstore.png.webp" alt="apple store"></a>
+        </div>
+    </div>
+</div>
+    <div id="sk_last">
+        <div  class="sk_sk">
+            <a href="###">Elite Membership</a>  <br>
+            <a href="###">Coach Mode</a>        <br>
+            <a href="###">Sign Up</a>           <br>
+            <a href="###">Login</a>             <br>
+        </div>
+        <div class="sk_sk">
+            <a href="###">Workout Plans</a>     <br>
+            <a href="###">Exercise Database</a> <br>
+            <a href="###">Help Center</a>       <br>
+        </div>
+        <div class="sk_sk">
+            <a href="###">Blog</a>              <br>
+            <a href="###">Facebook</a>          <br>
+            <a href="###">Twitter</a>           <br>
+            <a href="###">Instagram</a>         <br>
+        </div>
+        <div class="sk_sk">
+            <a href="###">Privacy Policy</a>    <br>
+            <a href="###">Terms of Use</a>      <br>
+            <a href="###">IP/DMCA Notices</a>   <br>
+            <a href="###">Press & Media</a>     <br>
+        </div>
+        
+    </div>
+    <div id="sk_rights">
+        <p>Copyright © 2010-2022 Jefit, Inc.</p>
+        <p>All Rights Reserved.</p>
+        <a href="###" class="sk_gotopbtn"><i class="fa-solid fa-angle-up"></i></a>
+    </div>`;
+}
+
+export default footerLinks;
