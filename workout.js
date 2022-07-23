d8c97ebfa3ebaf2a26ffac4a34e20a9d331778ec
globalmamtamishra <101391863+globalmamtamishra@users.noreply.github.com>
2022-07-23 19:15:16 +0530

    Add files via upload
    done

---
+++ workout.js
@@ -0,0 +1,253 @@
+let dat=[ {
+    "img_src": "https://cdn.jefit.com/uc/file/0b6a31438722e97f/1.jpg",
+    "Name": "Dynamic Stretching Series",
+    "Name 2": "Jefit Holiday Bodyweight Workout #2",
+    "Frequency": "3 days",
+    "Category": "Category:Maintaining",
+    "Exp_Level": "Exp_Level:Beginner",
+    "Views_Downloads": "8/0",
+    "created_by": "halleya",
+    "EliteFree": "Free",
+    "vipicon": "vipicon:VIP"
+    
+},
+{
+    "img_src": "https://cdn.jefit.com/uc/file/9418dc57b750dac9/1.jpg",
+    "Name": "Stronger Legs & Core",
+    "Name 2": "Post Injury Plan: Regaining Back Strength",
+    "Frequency": "3 days",
+        "Category": "Category:Maintaining",
+       
+        "Exp_Level": "Beginner",
+        "Views_Downloads": "14/1",
+        "created_by": "mbiegun",
+        "EliteFree": "Free",
+        "vipicon": "VIP"
+},
+{
+    "img_src": "https://cdn.jefit.com/uc/file/87776aa7ba1d06db/1.jpg",
+    "Name": "Leg Workout: Glute Focused",
+    "Name 2": "Fit to the Core",
+    "Frequency": "3 days",
+    "Category": "Maintaining",
+    "Exp_Level": "Beginner",
+    "Views_Downloads": "8/0",
+    "created_by": "halleya",
+    "EliteFree": "Free",
+    "vipicon": "VIP"
+},
+{
+    "img_src": "https://cdn.jefit.com/uc/file/c6809df063cf0f08/1.jpg",
+    "Name": "5 Day Muscle Mass Split",
+    "Name 2": "Body Weight Home Circuit Program - L 3",
+    "Frequency": "4 days",
+        "Category": "Bulking",
+        "Exp_Level": "Beginner",
+        "Views_Downloads": "7/0",
+        "created_by": "chalonlubin",
+        "EliteFree": "Free",
+        "vipicon": ""
+},
+{
+    "img_src": "https://cdn.jefit.com/uc/file/469e81200dbf6272/1.jpg",
+    "Name": "1-Day Kettlebell Workout",
+    "Name 2": "Lacrosse Strength Training",
+    "Frequency": "3 days",
+    "Category": "Category:Maintaining",
+    "Exp_Level": "Exp_Level:Beginner",
+    "Views_Downloads": "8/0",
+    "created_by": "halleya",
+    "EliteFree": "Free",
+    "vipicon": "vipicon:VIP"
+},
+{
+    "img_src": "https://cdn.jefit.com/uc/file/e53b0c5df567a260/1.jpg",
+    "Name": "HIIT Program: Rowing Build-Up",
+    "Name 2": "Barbell Leg Routine",
+    "Frequency": "3 days",
+    "Category": "Category:Maintaining",
+    "Exp_Level": "Exp_Level:Beginner",
+    "Views_Downloads": "8/0",
+    "created_by": "halleya",
+    "EliteFree": "Free",
+    "vipicon": "vipicon:VIP"
+    
+},
+{
+    "img_src": "https://cdn.jefit.com/uc/file/c2f58ad6f17797a3/1.jpg",
+    "Name": "From Fat to Fit (3-Month Plan)",
+    "Name 2": "Get Rid Of That Muffin Top",
+    "Frequency": "3 days",
+    "Category": "Category:Maintaining",
+    "Exp_Level": "Exp_Level:Beginner",
+    "Views_Downloads": "8/0",
+    "created_by": "halleya",
+    "EliteFree": "Free",
+    "vipicon": "vipicon:VIP"
+},
+{
+    "img_src": "https://cdn.jefit.com/uc/file/2cc35ec18e87de63/1.jpg",
+    "Name": "Total Body Strength (Phase 2)",
+    "Name 2": "Machine-based Strength Program",
+
+    "Frequency": "2 days",
+    "Category": "Category:Maintaining",
+    "Exp_Level": "Exp_Level:Beginner",
+    "Views_Downloads": "8/0",
+    "created_by": "halleya",
+    "EliteFree": "Free",
+    "vipicon": "vipicon:VIP"
+
+
+},
+{
+    "img_src": "https://cdn.jefit.com/uc/file/4e6837f6cba13dea/1.jpg",
+    "Name": "8 Exercise Push/Pull Routine",
+    "Name 2": "Arms & Core Day",
+    "Frequency": "3 days",
+    "Category": "Category:Maintaining",
+    "Exp_Level": "Exp_Level:Beginner",
+    "Views_Downloads": "8/0",
+    "created_by": "halleya",
+    "EliteFree": "Free",
+    "vipicon": "vipicon:VIP"
+    
+},
+{
+    "img_src": "https://cdn.jefit.com/uc/file/2d9a1fc648d514f2/1.jpg",
+    "Name": "Smith Machine Upper Body Routine",
+    "Name 2": "Football Training",
+
+    "Frequency": "2 days",
+    "Category": "Category:Maintaining",
+    "Exp_Level": "Exp_Level:Beginner",
+    "Views_Downloads": "8/0",
+    "created_by": "halleya",
+    "EliteFree": "Free",
+    "vipicon": "vipicon:VIP"
+
+},
+{
+    "img_src": "https://cdn.jefit.com/uc/file/9418dc57b750dac9/1.jpg",
+    "Name": "Get Strong with Supersets",
+    "Name 2": "Metabolic Conditioning (Week 3-4)",
+
+    "Frequency": "2 days",
+    "Category": "Category:Maintaining",
+    "Exp_Level": "Exp_Level:Beginner",
+    "Views_Downloads": "8/0",
+    "created_by": "halleya",
+    "EliteFree": "Free",
+    "vipicon": "vipicon:VIP"
+
+},
+{
+    "img_src": "https://cdn.jefit.com/uc/file/9caa1c968aeebe1c/1.jpg",
+    "Name": "Core Focused",
+    "Name 2": "Shoulder Strength (Post-Rehab)",
+    "Frequency": "2 days",
+    "Category": "Category:Maintaining",
+    "Exp_Level": "Exp_Level:Beginner",
+    "Views_Downloads": "8/0",
+    "created_by": "halleya",
+    "EliteFree": "Free",
+    "vipicon": "vipicon:VIP"
+
+}
+]
+
+
+
+
+
+// starting
+// let div=document.createElement("div")
+// let img=document.createElement("img")
+// let nam=document.createElement("p")
+// let i=0
+   
+//   setInterval(function(){
+//    if(i==dat.length){
+//        i=0
+//    }
+//         img.src=dat[i].img_src
+// nam.innerText=dat[i].Name
+
+
+
+//         console.log(i)
+//       i++
+//   },1000)
+//   div.append(img,nam)
+
+ 
+//   document.querySelector("#workoutappend").append(div)
+
+// ending
+
+
+
+var Data = JSON.parse(localStorage.getItem("basket")) || [];
+append(dat)
+function append(dat){
+document.getElementById("workoutappend").innerHTML=null;
+
+//container.innerHTML=null
+dat.forEach(function(el){
+    let div=document.createElement("div")
+
+    let img=document.createElement("img")
+    
+    img.src=el.img_src
+
+    let name=document.createElement("h2")
+
+    name.innerText=el.Name
+
+
+    div.append(img,name)
+
+    div.setAttribute("class","boxmamta");
+
+    
+
+    var add = {
+        img:el.img_src,
+        name: el.Name,
+
+
+        Frequency:el.Frequency,
+        Category: el.Category,
+        Exp_Level: el.Exp_Level,
+        Views_Downloads: el.Views_Downloads,
+        created_by: el.created_by,
+        EliteFree: el.EliteFree,
+        vipicon: el.vipicon
+        
+      }
+
+      div.onclick = () => {
+        addtonext(add);
+      };
+
+      document.querySelector("#workoutappend").append(div);
+ 
+   
+})
+
+}
+
+let  addtonext= (add) => {
+    console.log(add);
+       localStorage.setItem("individual",JSON.stringify(add));
+       window.location.href="mainpage.html";
+  };
+
+
+
+
+
+
+
+
+
