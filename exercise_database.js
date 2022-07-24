88410c714664f21092764776561aeedac9eb2b8c
Sushant Kumar <101393454+sushantkr961@users.noreply.github.com>
2022-07-24 10:23:31 +0530

    Add files via upload

---
+++ exercise_database.js
@@ -0,0 +1,119 @@
+import footerLinks from "./footer/footer store links.js";
+    // console.log(footerLinks());
+    document.getElementById("footer").innerHTML = footerLinks();
+
+    import navbar from "./navbar/navbar.js";
+    console.log(navbar());
+    document.getElementById("sks_navba").innerHTML = navbar();
+
+
+
+
+import exercises from "./components/data.js";
+let newexercises=[];
+
+
+for(let i=0;i<12;i++){
+    newexercises.push(exercises[i]);
+}
+
+let obj=JSON.parse(localStorage.getItem("exercisedata")) || {
+    url:"https://www.jefit.com/images/exercises/800_600/2912.jpg",
+    url1:"https://www.jefit.com/images/exercises/800_600/2913.jpg",
+    url2:"https://www.jefit.com/images/exercises/b3.jpg",
+    name:"Cable Incline Press",
+    mmg:"Chest",
+    omg:"Shoulders",
+    type:"Strength",
+    machine:"Compound",
+    equipment:"Bench",
+    difficulty:"Beginner",
+};
+
+let display=(obj)=>{
+    let img1=document.createElement("img");
+    let p1=document.createElement("p");
+    p1.innerText="Click to Enlarge";
+    p1.setAttribute("class","p");
+    let p2=document.createElement("p");
+    p2.innerText="Click to Enlarge";
+    p2.setAttribute("class","p");
+    let p3=document.createElement("p");
+    p3.innerText="1";
+    p3.setAttribute("class","p");
+    let p4=document.createElement("p");
+    p4.innerText="2";
+    p4.setAttribute("class","p");
+    img1.src=obj.url;
+    let img2=document.createElement("img");
+    img2.src=obj.url1;
+    console.log(obj.url);
+    document.getElementById("imgone").append(p3,img1,p1);
+    document.getElementById("imgtwo").append(p4,img2,p2);
+
+    let mmg1=document.createElement("h4");
+    mmg1.innerText="Main Muscle Group: ";
+    let mmg2=document.createElement("p");
+    mmg2.innerText=obj.mmg;
+    let omg1=document.createElement("h4");
+    omg1.innerText="Other Muscle Group: ";
+    let omg2=document.createElement("p");
+    omg2.innerText=obj.omg;
+    let t1=document.createElement("h4");
+    t1.innerText="Type: ";
+    let t2=document.createElement("p");
+    t2.innerText=obj.type;
+    let m1=document.createElement("h4");
+    m1.innerText="Machines: ";
+    let m2=document.createElement("p");
+    m2.innerText=obj.machine;
+    let e1=document.createElement("h4");
+    e1.innerText="Equipment";
+    let e2=document.createElement("p");
+    e2.innerText=obj.equipment;
+    let d1=document.createElement("h4");
+    d1.innerText="Difficulty: ";
+    let d2=document.createElement("p");
+    d2.innerText=obj.difficulty;
+    document.getElementById("details").append(mmg1,mmg2,omg1,omg2,t1,t2,m1,m2,e1,e2,d1,d2);
+
+
+    let btn1=document.createElement("button");
+    btn1.innerText="Track My Progress";
+    let btn2=document.createElement("button");
+    btn2.innerText="+Add To Routine";
+    let btn3=document.createElement("button");
+    btn3.innerText="Record Logs";
+    document.getElementById("buttons").append(btn1,btn2,btn3);
+
+
+    let img3=document.createElement("img");
+    img3.src=obj.url2;
+    document.getElementById("sumidleftimgthird").append(img3);
+
+    let h3=document.createElement("h3")
+    h3.innerText=obj.name+" Images";
+    document.getElementById("tophead").append(h3);
+}
+
+let displayexercises=()=>{
+    newexercises.forEach((el)=>{
+    let box=document.createElement("div");
+    let img=document.createElement("img");
+    img.src=el.url;
+    let name=document.createElement("p");
+    name.innerText=el.name;
+    name.setAttribute("class","name");
+    box.append(img,name);
+    document.getElementById("sulowerup").append(box);
+    })
+}
+document.getElementById("star").addEventListener("click",()=>{
+    let star=document.createElement("img");
+    document.getElementById("star").innerText=null;
+    star.src="https://p.kindpng.com/picc/s/729-7296605_golden-star-png-free-pic-gold-star-transparent.png";
+    document.getElementById("star").append(star);
+})
+
+displayexercises();
+display(obj);
---
+++ footer/footer store links.js
@@ -0,0 +1,49 @@
+function footerLinks() {
+    return `<div id="footer_links">
+    <div id="sks_outer">
+        <div id="sk_txt">
+            <span>GET OUR MOBILE APP</span>
+            <h2>Download and start your workout journey</h2>
+        </div>
+        <div id="sk_qr">
+            <img src="https://cdn.jefit.com/wp/wp-content/uploads/2021/12/qrcode_only.png">
+        </div>
+        <div id="sk_li">
+            <a href="https://play.google.com/store/apps/details?id=je.fit&referrer=utm_source%3Demail"><img src="https://cdn.jefit.com/wp/wp-content/uploads/2021/11/googleplay.png.webp" alt="play store"></a>
+            <a href="https://apps.apple.com/app/apple-store/id449810000"><img src="https://cdn.jefit.com/wp/wp-content/uploads/2021/11/appstore.png.webp" alt="apple store"></a>
+        </div>
+    </div>
+</div>
+    <div id="sk_last">
+        <div  class="sk_sk">
+            <a href="ElitePage.html">Elite Membership</a>  <br>
+            <a href="coach.html">Coach Mode</a>        <br>
+            <a href="signup.html">Sign Up</a>           <br>
+            <a href="login.html">Login</a>             <br>
+        </div>
+        <div class="sk_sk">
+            <a href="workout.html">Workout Plans</a>     <br>
+            <a href="exercise_guide.html">Exercise Database</a> <br>
+            <a href="###">Help Center</a>       <br>
+        </div>
+        <div class="sk_sk">
+            <a href="blog.html">Blog</a>              <br>
+            <a href="https://www.facebook.com/jefitapp">Facebook</a>          <br>
+            <a href="https://twitter.com/jefitinc">Twitter</a>           <br>
+            <a href="https://www.instagram.com/jefitapp/">Instagram</a>         <br>
+        </div>
+        <div class="sk_sk">
+            <a href="Privacy policy.html">Privacy Policy</a>    <br>
+            <a href="Terms of service.html">Terms of Use</a>      <br>
+            <a href="Refund policy.html">Refund Policy</a>   <br>
+            <a href="Privacy policy.html">Privacy policy</a>     <br>
+        </div>
+        
+    </div>
+    <div id="sk_rights">
+        <p>Copyright © 2010-2022 Jefit, Inc.</p>
+        <p>All Rights Reserved.</p>
+    </div>`;
+}
+
+export default footerLinks;
