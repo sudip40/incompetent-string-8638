46aef791736e478e37d8fdd2666c4c47e39ca340
sushantkr961 <sushantonly961@gmail.com>
2022-07-25 17:22:41 +0530

    wow

--- homepage.js
+++ homepage.js
@@ -32,46 +32,46 @@ import footerLinks from "./footer/footer store links.js";
 
 
 
-    let images = [
-        "./images/image1.png",
-        "./images/image2.png",
-        "./images/image3.png",
-        "./images/image4.png",
-        "./images/image5.png",
-        "./images/image6.png",
-        "./images/image7.png",
-        "./images/image8.png",
-        "./images/image9.png",
-        "./images/image10.png",
-        "./images/image11.png",
-        "./images/image12.png",
-        "./images/image13.png",
-        "./images/image14.png",
-        "./images/image15.png",
-        "./images/image16.png",
-        "./images/image17.png",
-        "./images/image18.png",
-        "./images/image19.png",
-        "./images/image20.png",
-    ];
-    let i=0;
-    function slideShow() {
-        document.querySelector("#sk_image").src = images[i];
-        document.querySelector("#sk_image1").src = images[i];
-        document.querySelector("#sk_image2").src = images[i];
-        document.querySelector("#sk_image3").src = images[i];
-        document.querySelector("#sk_image4").src = images[i];
-        document.querySelector("#sk_image5").src = images[i];
-        document.querySelector("#sk_image6").src = images[i];
-        document.querySelector("#sk_image7").src = images[i];
-        if (i<images.length-1){
-            i++;
-        } else {
-            i=0;
-        }
-        // console.log(i);
-    }
-    setInterval(slideShow,5000);
+    // let images = [
+    //     "./images/image1.png",
+    //     "./images/image2.png",
+    //     "./images/image3.png",
+    //     "./images/image4.png",
+    //     "./images/image5.png",
+    //     "./images/image6.png",
+    //     "./images/image7.png",
+    //     "./images/image8.png",
+    //     "./images/image9.png",
+    //     "./images/image10.png",
+    //     "./images/image11.png",
+    //     "./images/image12.png",
+    //     "./images/image13.png",
+    //     "./images/image14.png",
+    //     "./images/image15.png",
+    //     "./images/image16.png",
+    //     "./images/image17.png",
+    //     "./images/image18.png",
+    //     "./images/image19.png",
+    //     "./images/image20.png",
+    // ];
+    // let i=0;
+    // function slideShow() {
+    //     document.querySelector("#sk_image").src = images[i];
+    //     document.querySelector("#sk_image1").src = images[i];
+    //     document.querySelector("#sk_image2").src = images[i];
+    //     document.querySelector("#sk_image3").src = images[i];
+    //     document.querySelector("#sk_image4").src = images[i];
+    //     document.querySelector("#sk_image5").src = images[i];
+    //     document.querySelector("#sk_image6").src = images[i];
+    //     document.querySelector("#sk_image7").src = images[i];
+    //     if (i<images.length-1){
+    //         i++;
+    //     } else {
+    //         i=0;
+    //     }
+    //     // console.log(i);
+    // }
+    // setInterval(slideShow,5000);
 
 
 
@@ -80,8 +80,8 @@ import footerLinks from "./footer/footer store links.js";
 
     let photos = [
         "./photos/photo1.jpg",
-        "./photos/photo1.jpg",
-        "./photos/photo1.jpg",
+        "./photos/photo2.jpg",
+        "./photos/photo3.jpg",
     ];
     let j=0;
     function machao() {
